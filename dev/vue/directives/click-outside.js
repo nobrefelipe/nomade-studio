ab6181580534e805987e67c707029d872014b2ce
WMP Felipe <wmpfelipe@WMP-Felipes-MacBook-Pro.local>
2017-06-05 13:12:05 +0100

    added click-outside directive

---
+++ dev/vue/directives/click-outside.js
@@ -0,0 +1,39 @@
+
+/*
+    CLICK OUTSIDE ELEMENT DIRECTIVE
+*/
+
+
+import Vue from 'vue';
+
+Vue.directive('click-outside', {
+
+    bind(el, binding, vnode) {
+
+        var trigger = function (event) {
+
+            //check if user is clicking on the host element || any of its children
+            if (!(el == event.target || el.contains(event.target))) {
+
+                // FIRE IT!
+                vnode.context[binding.expression](event);
+
+            }
+
+        };
+
+        // WATCH FOR CLICKING AROUND
+        document.body.addEventListener('click', trigger);
+
+    },
+
+
+    unbind() {
+
+        //STOP WATCHING IT! #perfomanceMatters
+        document.body.removeEventListener('click', trigger);
+
+    }
+
+
+});
